64edece97048097d2e404ca1a7e7f1cf59d1e847
spungs <7564s@naver.com>
2026-01-02 22:29:27 +0900

    chore: force add config.js for deployment

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// Supabase 설정
+const config = {
+    supabaseUrl: 'https://dcgmgepcmkflkyvrxbdy.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjZ21nZXBjbWtmbGt5dnJ4YmR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0MjIxNDUsImV4cCI6MjA2NTk5ODE0NX0.R77KHkzIh1nyTnqITzWrd83MwiuCHNjMyLeZjUP1YKk'
+}; 
